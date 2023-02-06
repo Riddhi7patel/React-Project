ca6474d3deee4750cbf1db2b2af9a853fb2f94da
Riddhi Patel <Ridshah2@gmail.com>
2023-02-06 13:43:35 -0500

    router hooks

--- new repo/react-project/src/pages/films.page.jsx
+++ new repo/react-project/src/pages/films.page.jsx
@@ -1,9 +1,5 @@
 import { useState, useEffect } from "react";
-import {
-  filterFilmsByDirector,
-  getListOf,
-  getFilmsStats,
-} from "../helpers/film.helpers";
+import { filterFilmsByDirector, getListOf, getFilmStats } from "../helpers/film.helpers";
 import { Link } from "react-router-dom";
 
 function FilmsPage(props) {
@@ -11,7 +7,7 @@ function FilmsPage(props) {
   let [searchDirector, setSearchDirector] = useState("");
 
   function getFilms() {
-    fetch("https://studioghibliapi-d6fc8.web.app/films/${id}")
+    fetch("https://studioghibliapi-d6fc8.web.app/films")
       .then((response) => response.json())
       .then((films) => setList(films))
       .catch((error) => console.error(error));
@@ -23,53 +19,59 @@ function FilmsPage(props) {
 
   let filmsByDirector = filterFilmsByDirector(list, searchDirector);
   let directors = getListOf(list, "director");
-  let { avg_score, latest, total } = getFilmsStats(filmsByDirector);
+  let { avg_score, latest, total } = getFilmStats(filmsByDirector);
 
   return (
     <div>
-      <h1 className="center"><span className="studio">Studio Ghibli Films</span></h1>
+    <div className="filmHraderSection">
+      <h1 className="H1-center"><span className="studio">Studio Ghibli Films</span></h1>
       <form>
-        <label htmlFor="searchDirector">Filter By Director</label>
-        <select
-          name="searchDirector"
-          id="searchDirector"
-          value={searchDirector}
-          onChange={(e) => setSearchDirector(e.target.value)}
-        >
-          <option value="">All</option>
-          {directors.map((director, idx) => {
-            return (
+         <div className="form-group">
+             <label htmlFor="searchDirector">Filter By Director</label>
+             <select
+               name="searchDirector"
+               id="searchDirector"
+               value={searchDirector}
+               onChange={(e) => {
+                setSearchDirector(e.target.value);
+              }}
+             >
+               <option value="">All</option>
+              {directors.map((director, idx) => {
+               return (
               <option key={director + idx} value={director}>
                 {director}
-              </option>
-            );
-          })}
-        </select>
-      </form>
-      <div>
-        <div>
-          <span># Of Films</span>
-          <span>{total}</span>
+               </option>
+              );
+             })}
+              </select>
+          </div>
+        </form>
         </div>
-        <div>
-          <span>Average Rating</span>
-          <span>{avg_score.toFixed(2)}</span>
-        </div>
-        <div>
-          <span>Latest Film</span>
-          <span>{latest}</span>
-        </div>
-      </div>
+        <div className="director-data">
+  <div>
+    <span># Of Films</span>
+    <span>{total}</span>
+  </div>
+  <div>
+    <span>Average Rating</span>
+    <span>{avg_score.toFixed(2)}</span>
+  </div>
+  <div>
+    <span>Latest Film</span>
+    <span>{latest}</span>
+  </div>
+</div>
       <ul className="tiles">
-        {filmsByDirector.map((film) => {
+        {filmsByDirector.length !== 0 &&
+        filmsByDirector.map((film) => {
           return (
-            <li key={film.id}>
-              <Link to={`/films/${film.id}`}>
-                {film.title}</Link>
-                {/* <a href={`${film.movie_banner}`}>
-                  <img src={`${film.image}`} alt="Film Poster" />
-                </a> */}
-            </li>
+          <li key={film.id}>
+            <Link to={`/film/${film.id}`}>
+              <h2>{film.title}</h2>
+            </Link>
+            <img src={`${film.image}`} alt="Film Poster" />
+          </li>
           );
         })}
       </ul>
